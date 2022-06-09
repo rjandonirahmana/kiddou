66c2dc880adb2dc0d28b1dc2a0753e84453d9151
rjandonirahmana <rjandonirahmana@gmail.com>
2022-06-09 09:40:06 +0700

    subscription and renew

--- domain/videos.go
+++ domain/videos.go
@@ -17,11 +17,21 @@ type Videos struct {
 }
 
 type Subscribers struct {
+	ID               int
 	UserID           string
 	VideoID          int
 	TypeSubscription string
+	SubscribeAT      time.Time
 	ExpiredAt        time.Time
 	Status           string
+}
+
+type SubsriptionStatusDTO struct {
+	VideoName        string `json:"video_name"`
+	TypeSubscription string `json:"type_subscription"`
+	ExpiredAt        string `json:"expired_at"`
+	SubscribeAT      string `json:"subscribe_at"`
+	Status           string `json:"status"`
 }
 
 type InserVideosRequest struct {
@@ -35,8 +45,19 @@ type RepoVideos interface {
 	InsertVideos(ctx context.Context, tx *sql.Tx, video *Videos) error
 	GetByCategory(ctx context.Context, categoryID int) (res []Videos, err error)
 	GetAllCategories(ctx context.Context) (res []Categories, err error)
+	UpdateVideo(ctx context.Context, tx *sql.Tx, video *Videos) error
+	GetByID(ctx context.Context, ID int) (res Videos, err error)
 }
 
 type UsecaseVideos interface {
 	InsertVideos(ctx context.Context, videos []byte, input *InserVideosRequest) error
+	SubscribtionVideo(ctx context.Context, userID string, videoID int) error
+	SubsribesStatus(ctx context.Context, userID string, videoID int) (res SubsriptionStatusDTO, err error)
+}
+
+type SubscribersRepo interface {
+	InsertSub(ctx context.Context, tx *sql.Tx, input *Subscribers) error
+	UpdateSub(ctx context.Context, tx *sql.Tx, input *Subscribers) error
+	GetByVideoID(ctx context.Context, ID int, userID string) (res Subscribers, err error)
+	GetByUserID(ctx context.Context, userID string) (res []Subscribers, err error)
 }
